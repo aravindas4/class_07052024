cb0308bc8202297741956b935050fd555963c4d6
aravindas4 <ash.g.education@gmail.com>
2024-05-09 00:19:40 +0530

    Add class tuts

---
+++ hw2/src/main/java/in/aravinda_holla/hw2/MyFirstAPI.java
@@ -0,0 +1,31 @@
+package in.aravinda_holla.hw2;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstAPI {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello Class 1!";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String sayHelloName(@PathVariable("name") String name) {
+        return "Hello " + name ;
+    }
+
+    @RequestMapping("/hello/{name}/{times}")
+    public String sayHelloNameTimes(@PathVariable("name") String name,
+                                    @PathVariable("times") int times) {
+        String str = "";
+        for (int i = 0; i < times; i++) {
+            str += "Hello " + name + " " + (i + 1);
+            str += "<br>";
+        }
+        return str;
+    }
+
+}
---
+++ hw2/src/test/java/in/aravinda_holla/hw2/Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aravinda_holla.hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
